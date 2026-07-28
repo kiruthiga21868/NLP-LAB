a271f27ac8208a9b1b6a666192a0e61abe5720df
Kiruthiga P <kiruthigavss21@gmail.com>
2026-07-28 13:25:21 +0530

    Added Experiment 1 - Tokenization, Stemming and Lemmatization

---
+++ Experiment1/Experiment1_Tokenization_Stemming_Lemmatization.py
@@ -0,0 +1,23 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('wordnet')
+text = input("Enter a sentence: ")
+tokens = word_tokenize(text)
+stemmer = PorterStemmer()
+stemmed_words = [stemmer.stem(word) for word in tokens]
+lemmatizer = WordNetLemmatizer()
+lemmatized_words = [lemmatizer.lemmatize(word) for word in tokens]
+print("\nOriginal Text:")
+print(text)
+print("\nTokens:")
+print(tokens)
+print("\nStemmed Words:")
+print(stemmed_words)
+print("\nLemmatized Words:")
+print(lemmatized_words)
+print("\nComparison:")
+print("Stemming reduces words to root forms, which may not be meaningful.")
+print("Lemmatization converts words to meaningful base forms.")
